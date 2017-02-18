b015ffff62251519fa8539e1bd98a851d9a796a0
JayXu <xuweijay@163.com>
2017-02-18 20:51:13 +0800

    githubDesktop

--- group15/【1511】徐伟杰/src/Demo.java
+++ group15/【1511】徐伟杰/src/Demo.java
@@ -1,7 +1,11 @@
+import java.util.Date;
 
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("Hahahah..");
 		//HeHeda
+		//test
+		Date d = new Date();
+		System.out.println(d.toLocaleString());
 	}
 }
