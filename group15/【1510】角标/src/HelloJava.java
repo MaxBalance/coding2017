5ca3b389fc018f0b08a01cc342114c24d14e74a9
brucejiao1 <jiao.xiaopan@163.com>
2017-02-19 19:11:41 +0800

    test01
    the first test about github

---
+++ group15/【1510】角标/src/HelloJava.java
@@ -0,0 +1,8 @@
+/**
+ * Created by dell on 2017/2/19.
+ */
+public class HelloJava {
+    public static void main(String[] args){
+        System.out.print("first test about coding 2017");
+    }
+}
